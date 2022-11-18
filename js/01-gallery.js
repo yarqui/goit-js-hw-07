1279e360872d59d5a07e8a0551d2f706abe32e5e
yarqui <108986486+yarqui@users.noreply.github.com>
2022-11-18 22:18:29 +0200

    1st done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryItems.map((item) => {
+  const { preview, original, description } = item;
+
+  const galleryItemMarkup = `<div class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+
+  galleryRef.insertAdjacentHTML("beforeend", galleryItemMarkup);
+});
+
+const onGalleryItemClick = (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const fullsizeImage = e.target.dataset.source;
+
+  const galleryImageModal = basicLightbox.create(`
+  <img src="${fullsizeImage}">
+  `);
+
+  galleryImageModal.show();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      galleryImageModal.close();
+    }
+  });
+};
+
+galleryRef.addEventListener("click", onGalleryItemClick);
