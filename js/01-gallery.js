cb67a4ec50256ab2f69f05a6e53d4f24a55da2a0
yarqui <y.pelykh@gmail.com>
2022-11-19 14:32:42 +0200

    2nd done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,6 @@ const galleryRef = document.querySelector(".gallery");
 
 galleryItems.map((item) => {
   const { preview, original, description } = item;
-
   const galleryItemMarkup = `<div class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
 
   galleryRef.insertAdjacentHTML("beforeend", galleryItemMarkup);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryItems.map((item) => {
+  const { preview, original, description } = item;
+
+  const galleryItemMarkup = `<li><a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`;
+
+  galleryRef.insertAdjacentHTML("beforeend", galleryItemMarkup);
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+lightbox.show();
